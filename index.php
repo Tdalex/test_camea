adfe54356391a24e2f6d1cac261b3df73fde657b
Dalex Tanguy <dalex.tanguy@gmail.com>
2019-04-05 11:09:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<body>
+<?php 
+if($_POST){
+	var_dump($_POST);
+}
+?>
+<form method='POST'>
+<input type="file" accept="image/*" capture="camera">
+<button type="submit">
+</form>
+</body>
+</html>
