4cdf47cf1c714e0d0fa269370a5f1aec441a6197
Dalex Tanguy <dalex.tanguy@gmail.com>
2019-04-08 11:38:04 +0200

    Add files via upload

--- video.js
+++ video.js
@@ -31,7 +31,6 @@ tick();
 var decodeCallback = function (ptr, len, resultIndex, resultCount) {
   var result = new Uint8Array(ZXing.HEAPU8.buffer, ptr, len);
   console.log(String.fromCharCode.apply(null, result));
-  alert(String.fromCharCode.apply(null, result));
   barcode_result.textContent = String.fromCharCode.apply(null, result);
   buttonGo.disabled = false;
   if (isPC) {
@@ -65,29 +64,6 @@ if (browserRedirect() == 'pc') {
   isPC = true;
 } else {
   isPC = false;
-}
-
-// stackoverflow: http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata/5100158
-function dataURItoBlob(dataURI) {
-  // convert base64/URLEncoded data component to raw binary data held in a string
-  var byteString;
-  if (dataURI.split(',')[0].indexOf('base64') >= 0)
-    byteString = atob(dataURI.split(',')[1]);
-  else
-    byteString = unescape(dataURI.split(',')[1]);
-
-  // separate out the mime component
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-  // write the bytes of the string to a typed array
-  var ia = new Uint8Array(byteString.length);
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  return new Blob([ia], {
-    type: mimeString
-  });
 }
 
 // add button event
